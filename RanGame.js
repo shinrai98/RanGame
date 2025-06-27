fffa93f0ecf133f11f38d2e8b9df97a7a58ade82
shinrai98 <95671448+shinrai98@users.noreply.github.com>
2025-06-27 14:57:15 +0900

    Update RanGame.js

--- RanGame.js
+++ RanGame.js
@@ -1,4 +1,36 @@
-function CharacterName (){
+//tự động phát nhạt thì user tương tác với web
+const bgm = document.getElementById("bgm");
+
+// Random Main Character Gif
+function setMainCharGif() {
+    const ranMainCharGif = Math.floor(Math.random() * 10 + 1);
+    document.getElementById("MainCharGif").src = `gif/main${ranMainCharGif}.gif`;
+}
+
+//Random Enemy Gif
+function setEnemyGif(){
+    const ranEnemyGif = Math.floor(Math.random() * 14 + 1);
+    document.getElementById("EnemyGif").src = `gif/monster${ranEnemyGif}.gif`; 
+}
+//Random BG gif
+function setBGGif(){
+    const ranBGGif = Math.floor(Math.random() * 2 + 1);
+    document.getElementById("BG").style.backgroundImage = `url('gif/bg${ranBGGif}.gif')`;
+    // document.getElementById("BG").style.backgroundImage = ` linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),url('gif/bg${ranBGGif}.gif')`;
+    document.getElementById("BG").style.backgroundSize = "100% 100%";
+    document.getElementById("BG").style.backgroundRepeat = "no-repeat";
+    document.getElementById("BG").style.backgroundPosition = "center";
+}
+
+function enableBGM(){
+    bgm.play();
+    document.removeEventListener('click', enableBGM);
+}
+
+document.addEventListener('click', enableBGM);
+
+//step1
+function CharacterName(){
 
     CharacterStats.Name = document.getElementById("NameInput").value.trim();
     if (CharacterStats.Name === ""){
@@ -22,6 +54,7 @@ function backToStep1(){
 
 }
 
+//step2
 function RanSta(){
     CharacterStats.HP = Math.floor(Math.random() * (10 - 5 + 1 ) + 5);
     CharacterStats.Atk = Math.floor(Math.random() * (5 -1 + 1 ) + 1);
@@ -29,7 +62,7 @@ function RanSta(){
 
     if (CharacterStats.HP == 10 && CharacterStats.Atk == 5 && CharacterStats.Def == 5 ){
         alert = "Thật tuyệt vời , bạn đã roll ra nhân vật với chỉ số khởi đầu tốt nhất"
-    } else {
+    } else if (CharacterStats.HP == 5 && CharacterStats.Atk == 1 && CharacterStats.Def == 1 ){
         alert = "úi cha, bạn đã roll ra nhân vật với chỉ số thấp nhất , nhưng đừng lo , bạn vẫn có thể tiếp tục cuộc hành trình với 1 chút khó khăn hơn với người thường thôi"
     }
 
@@ -46,47 +79,34 @@ function RanSta(){
 
 }
 
+//step3
 function GameStart(){
+    //thay đổi bgm
+    document.getElementById("bgm").src="bgm/Zambolino-Machine.mp3";
+    document.getElementById("bgm").play();
+
     document.getElementById("step3").style.display = "none";
     document.getElementById("step4").style.display = "block";
+
+    setMainCharGif()
 
     document.querySelectorAll(".showName").forEach(el => el.textContent = CharacterStats.Name);
     document.querySelectorAll(".showHP").forEach(el => el.textContent = CharacterStats.HP);
     document.querySelectorAll(".showAtk").forEach(el => el.textContent = CharacterStats.Atk);
     document.querySelectorAll(".showDef").forEach(el => el.textContent = CharacterStats.Def);
 
-    RName = Math.floor(Math.random() * (3 -1 + 1 ) + 1);
-    RGif = Math.floor(Math.random() * (3 -1 + 1 ) + 1);
+    const REnemyName = Math.floor(Math.random() * EnemyName.length);
     EnemyStats.HP = Math.floor(Math.random() * (10 - 5 + 1 ) + 5);
     EnemyStats.Atk = Math.floor(Math.random() * (5 -1 + 1 ) + 1);
     EnemyStats.Def = Math.floor(Math.random() * (5 -1 + 1 ) + 1);
 
-    switch (RName){
-        case 1 :
-            EnemyStats.Name = "Sasuke";
-            break;
-        case 2 :
-            EnemyStats.Name = "Naruto";
-            break;
-        case 3:
-            EnemyStats.Name = "Obito";
-            break;
-    }
+    setEnemyGif()
 
-    switch (RGif){
-        case 1:
-            document.getElementById("RanGif").src = "gif/monster1.gif";
-            break;
-        case 2:
-            document.getElementById("RanGif").src = "gif/monster2.gif";
-            break;
-        case 3:
-            document.getElementById("RanGif").src = "gif/monster3.gif";
-            break;
-    }
-
-    document.querySelectorAll(".showEName").forEach(el => el.textContent = EnemyStats.Name);
+    document.querySelectorAll(".showEName").forEach(el => el.textContent = EnemyName[REnemyName]);
     document.querySelectorAll(".showEHP").forEach(el => el.textContent = EnemyStats.HP);
     document.querySelectorAll(".showEAtk").forEach(el => el.textContent = EnemyStats.Atk);
     document.querySelectorAll(".showEDef").forEach(el => el.textContent = EnemyStats.Def);
+
+    setBGGif()
 }
+
