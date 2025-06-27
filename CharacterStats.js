67f2bc9e2993f189e13f20d5e28e912661a1674e
shinrai98 <95671448+shinrai98@users.noreply.github.com>
2025-06-27 14:57:32 +0900

    Update CharacterStats.js

--- CharacterStats.js
+++ CharacterStats.js
@@ -6,8 +6,12 @@ const CharacterStats = {
 }
 
 const EnemyStats = {
-    Name : "",
     HP : 0,
     Atk : 0,
     Def : 0
 }
+
+const EnemyName = ["Dreadfang","Nightgloom","Voidreaper","Abysscaller","Gravemaw","Soulgnawer","Bloodshade",
+                   "Gorgulax","Drakmorne","Fenbark","Bal'/Zorath","Kragnar","Thornvyrm","Zephiron",
+                    "Mechagon","Codebane","Nullframe","SynapseX","Bytefiend","Steelhowl",
+                    "Plagrowth","Fleshmorph","Spinecreep","Blisterspawn","Veinhost"]
