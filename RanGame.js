0ef74830472103c94b00174ac7c105d6037c17a4
shinrai98 <95671448+shinrai98@users.noreply.github.com>
2025-06-30 17:48:23 +0900

    Update RanGame.js

--- RanGame.js
+++ RanGame.js
@@ -1,82 +1,68 @@
-//tự động phát nhạt thì user tương tác với web
+//tự động phát nhạc khi user tương tác với web
 const bgm = document.getElementById("bgm");
 
-// Random Main Character Gif
-function setMainCharGif() {
-    const ranMainCharGif = Math.floor(Math.random() * 10 + 1);
-    document.getElementById("MainCharGif").src = `gif/main${ranMainCharGif}.gif`;
+let killCount = 0;
+let REnemyName
+
+//Hàm hổ trợ chung
+function rand(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function updateStatsDisplay(target, stats){
+    document.querySelectorAll(`.show${target}Name`).forEach(el => el.textContent = stats.Name);
+    document.querySelectorAll(`.show${target}HP`).forEach(el => el.textContent = stats.HP);
+    document.querySelectorAll(`.show${target}Atk`).forEach(el => el.textContent = stats.Atk);
+    document.querySelectorAll(`.show${target}Def`).forEach(el => el.textContent = stats.Def);
 }
 
-//Random Enemy Gif
-function setEnemyGif(){
-    const ranEnemyGif = Math.floor(Math.random() * 14 + 1);
-    document.getElementById("EnemyGif").src = `gif/monster${ranEnemyGif}.gif`; 
-}
-//Random BG gif
-function setBGGif(){
-    const ranBGGif = Math.floor(Math.random() * 2 + 1);
-    document.getElementById("BG").style.backgroundImage = `url('gif/bg${ranBGGif}.gif')`;
-    // document.getElementById("BG").style.backgroundImage = ` linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),url('gif/bg${ranBGGif}.gif')`;
-    document.getElementById("BG").style.backgroundSize = "100% 100%";
-    document.getElementById("BG").style.backgroundRepeat = "no-repeat";
-    document.getElementById("BG").style.backgroundPosition = "center";
-}
-
-function enableBGM(){
-    bgm.play();
-    document.removeEventListener('click', enableBGM);
-}
-
-document.addEventListener('click', enableBGM);
-
-//step1
+//step1 
+// nhận input tên và gán vào file CharacterStats
 function CharacterName(){
 
-    CharacterStats.Name = document.getElementById("NameInput").value.trim();
-    if (CharacterStats.Name === ""){
-        alert ("Vui lòng nhập tên nhân vật !!");
-        return;
-    }
+    const nameInput = document.getElementById("NameInput").value.trim();
+    if (nameInput === "") return alert ("Vui lòng nhập tên nhân vật !!");
 
     // Chuyển sang bước 2
+    CharacterStats.Name = nameInput;
     document.getElementById("step1").style.display= "none";
     document.getElementById("step2").style.display= "block";
-    document.querySelectorAll(".showName").forEach(el => el.textContent = CharacterStats.Name);
-    
+    updateStatsDisplay("", CharacterStats);
 }
 
-function backToStep1(){
-    document.getElementById("step2").style.display = "none";
-    document.getElementById("step3").style.display = "none";
-    document.getElementById("step1").style.display = "block";
+//Set main character stats
+function setMainCharStats() {
+    CharacterStats.HP = rand(5, 8);
+    CharacterStats.Atk = rand(1, 5);
+    CharacterStats.Def = rand(1, 5);
 
-    document.getElementById("NameInput").value = "";
-
+    const totalStats = CharacterStats.HP + CharacterStats.Atk + CharacterStats.Def ;
+    if (totalStats == 18 ){
+        alert ( " 🎉 Thật tuyệt vời , bạn đã roll ra nhân vật với chỉ số khởi đầu tốt nhất" );
+    } else if (totalStats == 7 ){
+        alert ( "úi cha, bạn đã roll ra nhân vật với chỉ số thấp nhất , nhưng đừng lo , bạn vẫn có thể tiếp tục cuộc hành trình với 1 chút khó khăn hơn với người thường thôi");
+    }
 }
 
 //step2
 function RanSta(){
-    CharacterStats.HP = Math.floor(Math.random() * (10 - 5 + 1 ) + 5);
-    CharacterStats.Atk = Math.floor(Math.random() * (5 -1 + 1 ) + 1);
-    CharacterStats.Def = Math.floor(Math.random() * (5 -1 + 1 ) + 1);
-
-    if (CharacterStats.HP == 10 && CharacterStats.Atk == 5 && CharacterStats.Def == 5 ){
-        alert = "Thật tuyệt vời , bạn đã roll ra nhân vật với chỉ số khởi đầu tốt nhất"
-    } else if (CharacterStats.HP == 5 && CharacterStats.Atk == 1 && CharacterStats.Def == 1 ){
-        alert = "úi cha, bạn đã roll ra nhân vật với chỉ số thấp nhất , nhưng đừng lo , bạn vẫn có thể tiếp tục cuộc hành trình với 1 chút khó khăn hơn với người thường thôi"
-    }
+    setMainCharStats();
 
     // Chuyển sang bước 3
     document.getElementById("step2").style.display = "none";
     document.getElementById("step3").style.display = "block";
-    document.getElementById("step4").style.display = "none";
 
     // Gán kết quả
-    document.querySelectorAll(".showName").forEach(el => el.textContent = CharacterStats.Name);
-    document.querySelectorAll(".showHP").forEach(el => el.textContent = CharacterStats.HP);
-    document.querySelectorAll(".showAtk").forEach(el => el.textContent = CharacterStats.Atk);
-    document.querySelectorAll(".showDef").forEach(el => el.textContent = CharacterStats.Def);
+    updateStatsDisplay("", CharacterStats);
 
+}
+
+// set enemy name stats , gif , main character gif
+function setEnemy(){
+    REnemyName = rand(0, EnemyName.length - 1);
+    EnemyStats.Name = EnemyName[REnemyName];
+    EnemyStats.HP = rand(5, 8);
+    EnemyStats.Atk = rand(1, 5);
+    EnemyStats.Def = rand(1, 5);
 }
 
 //step3
@@ -89,80 +75,121 @@ function GameStart(){
     document.getElementById("step4").style.display = "block";
 
     setMainCharGif()
-
-    document.querySelectorAll(".showName").forEach(el => el.textContent = CharacterStats.Name);
-    document.querySelectorAll(".showHP").forEach(el => el.textContent = CharacterStats.HP);
-    document.querySelectorAll(".showAtk").forEach(el => el.textContent = CharacterStats.Atk);
-    document.querySelectorAll(".showDef").forEach(el => el.textContent = CharacterStats.Def);
-
-    const REnemyName = Math.floor(Math.random() * EnemyName.length);
-    EnemyStats.HP = Math.floor(Math.random() * (10 - 5 + 1 ) + 5);
-    EnemyStats.Atk = Math.floor(Math.random() * (5 -1 + 1 ) + 1);
-    EnemyStats.Def = Math.floor(Math.random() * (5 -1 + 1 ) + 1);
-
     setEnemyGif()
-
-    document.querySelectorAll(".showEName").forEach(el => el.textContent = EnemyName[REnemyName]);
-    document.querySelectorAll(".showEHP").forEach(el => el.textContent = EnemyStats.HP);
-    document.querySelectorAll(".showEAtk").forEach(el => el.textContent = EnemyStats.Atk);
-    document.querySelectorAll(".showEDef").forEach(el => el.textContent = EnemyStats.Def);
-
     setBGGif()
+    setEnemy()
+
+    updateStatsDisplay("", CharacterStats);
+    updateStatsDisplay("E", EnemyStats);
+}
+
+// Random Main Character Gif
+function setMainCharGif() {
+    const ranMainCharGif = Math.floor(Math.random() * 10 + 1);
+    document.getElementById("MainCharGif").src = `gif/main${ranMainCharGif}.gif`;
+}
+
+//Random Enemy Gif
+function setEnemyGif(){
+    const ranEnemyGif = Math.floor(Math.random() * 14 + 1);
+    document.getElementById("EnemyGif").src = `gif/monster${ranEnemyGif}.gif`; 
+}
+
+//Random BG gif
+function setBGGif(){
+    const ranBGGif = Math.floor(Math.random() * 2 + 1);
+    document.getElementById("BG").style.backgroundImage = `url('gif/bg${ranBGGif}.gif')`;
+    // document.getElementById("BG").style.backgroundImage = ` linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),url('gif/bg${ranBGGif}.gif')`;
+    document.getElementById("BG").style.backgroundSize = "100% 100%";
+    document.getElementById("BG").style.backgroundRepeat = "no-repeat";
+    document.getElementById("BG").style.backgroundPosition = "center";
+}
+
+//BGM
+function enableBGM(){
+    bgm.play();
+    document.removeEventListener('click', enableBGM);
+}
+document.addEventListener('click', enableBGM);
+
+// Nút quay lại khi xác nhận tên
+function backToStep1(){
+    document.getElementById("step2").style.display = "none";
+    document.getElementById("step3").style.display = "none";
+    document.getElementById("step1").style.display = "block";
+
+    document.getElementById("NameInput").value = "";
+
 }
 
 //combat
 function Combat(){
-    const yourRoll = Math.floor(Math.random() * 6 + 1);
-    const enemyRoll = Math.floor(Math.random() * 6 + 1);
-    
+    const yourRoll = rand(1, 6);
+    const enemyRoll = rand(1, 6);
+
+    let result = `🧍 Bạn roll ra : <b>${yourRoll}</b><br>👾 Kẻ địch roll ra : <b>${enemyRoll}</b><br>`;
+
     if ( yourRoll > enemyRoll ){
-        document.getElementById("dialogueBox").innerHTML = 
-        `🧍 Bạn roll ra : <b>${yourRoll}</b><br>👾 Kẻ địch roll ra : <b>${enemyRoll}</b><br> Bạn lớn hơn nên bạn được ra đòn`;
-        setTimeout(YourAttack, 2500); // 2.5 giây
+        result += " Bạn được ra đòn";
+        document.getElementById("dialogueBox").innerHTML = result;
+        setTimeout(YourAttack, 2000);
     } else if (yourRoll < enemyRoll){ 
-        document.getElementById("dialogueBox").innerHTML = 
-        `🧍 Bạn roll ra : <b>${yourRoll}</b><br>👾 Kẻ địch roll ra : <b>${enemyRoll}</b><br> Kẻ địch lớn hơn nên kẻ địch được ra đòn`;
-        setTimeout(EnemyAttack, 2500); // 2.5 giây
+        result += " Kẻ địch được ra đòn";
+        document.getElementById("dialogueBox").innerHTML = result;
+        setTimeout(EnemyAttack, 2000);
     } else {
-        document.getElementById("dialogueBox").innerHTML = " Hãy roll lại !!"
+        document.getElementById("dialogueBox").innerHTML = " Hòa! Hãy roll lại !!";
     }
 
 }
+
 // Attack
 function YourAttack(){
-    if (CharacterStats.Atk > EnemyStats.Def ){
-        const Damage = CharacterStats.Atk - EnemyStats.Def;
-        EnemyStats.HP -= Damage;
-        document.getElementById("dialogueBox").innerHTML = 
-        "Bạn gây ra <span class = 'text-danger'>" + Damage + " Damge </span> cho địch . Kẻ địch còn <span class='text-success'>" + EnemyStats.HP + " HP </span>";
-        document.querySelectorAll(".showEHP").forEach(el => el.textContent = EnemyStats.HP);
-    } else{
-        EnemyStats.HP -= 1;
-        document.getElementById("dialogueBox").innerHTML =
-        "Bạn gây ra <span class = 'text-danger'> 1 Damage </span> cho địch . Kẻ địch còn <span class='text-success'>" + EnemyStats.HP + " HP </span>";
-        document.querySelectorAll(".showEHP").forEach(el => el.textContent = EnemyStats.HP);
+    const damage = Math.max(CharacterStats.Atk - EnemyStats.Def, 1);
+    EnemyStats.HP -= damage;
+
+    document.getElementById("dialogueBox").innerHTML = 
+        `💥 Bạn gây <span class="text-danger">${damage} damage </span> . Địch còn <span class="text-success">${EnemyStats.HP} HP</span>`;
+        updateStatsDisplay("E", EnemyStats);
+
+        if ( EnemyStats.HP <= 0){
+            killCount ++;
+            if ( killCount === 2){
+                alert("🎉Chúc mừng bạn !! bạn đã chiến thắng ! Quay trở lại để bắt đầu cuộc hành trình mới nào");
+                location.assign("index.html");
+            } else {
+                setTimeout(()=> {
+                    alert ("🎉 Kẻ địch đã bị hạ! Chuẩn bị đối mặt kẻ tiếp theo !!");
+                nextEnemy();
+                }, 1000)
+            }
+        }
     }
 
-    if ( EnemyStats.HP <= 0 ){
-        setTimeout(() => alert("🎉 Kẻ địch đã bị hạ! Chúc mừng bạn!"), 500);
+function EnemyAttack(){
+    const damage = Math.max(EnemyStats.Atk - CharacterStats.Def, 1);
+    CharacterStats.HP -= damage;
+
+    document.getElementById("dialogueBox").innerHTML =
+        ` Địch gây <span class="text-danger">${damage} damage </span>. Bạn còn <span class="text-success">${EnemyStats.HP} HP</span>`;
+        updateStatsDisplay("", CharacterStats);
+
+    if (CharacterStats.HP <= 0){
+        document.getElementById("dialogueBox").style.display = "none";
+        document.getElementById("gameOver").style.display = "block";
+        setTimeout(()=> {
+            alert("💀 Bạn đã thua ! bạn phải bắt đầu lại từ đầu");
+            location.assign("index.html");
+        }, 1500);
     }
 }
-function EnemyAttack(){
-    if (EnemyStats.Atk > CharacterStats.Def ){
-        const Damage = EnemyStats.Atk - CharacterStats.Def;
-        CharacterStats.HP -= Damage;
-        document.getElementById("dialogueBox").innerHTML =
-        "Kẻ địch gây ra <span class = 'text-danger'>" + Damage + " Damage </span> cho bạn . Bạn còn <span class='text-success'>" + CharacterStats.HP + " HP</span>";
-        document.querySelectorAll(".showHP").forEach(el => el.textContent = CharacterStats.HP);
-    } else{
-        CharacterStats.HP -= 1;
-        document.getElementById("dialogueBox").innerHTML =
-        "Kẻ địch gây ra <span class = 'text-danger'> 1 Damge </span> cho bạn . Bạn còn <span class='text-success'>" + CharacterStats.HP + " HP </span>";
-        document.querySelectorAll(".showHP").forEach(el => el.textContent = CharacterStats.HP);
-    }
 
-    if (CharacterStats.HP <= 0) {
-        //document.getElementById("gameOver").src = "gif/gameover1.gif";
-        setTimeout(() => alert("💀 Bạn đã thua trận!"), 500);
-    }
+// Next Enemy
+function nextEnemy(){
+    setEnemyGif();
+    setBGGif();
+    setEnemy();
+
+    updateStatsDisplay("E", EnemyStats);
+    document.getElementById("dialogueBox").innerHTML = "⚔️ Kẻ địch mới đã xuất hiện! Chúc bạn roll may mắn !";
 }
